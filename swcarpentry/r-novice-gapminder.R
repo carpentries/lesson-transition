e6f9322e824e6998a83e6d89b55d047b85f8241d
Zhian N. Kamvar <zkamvar@gmail.com>
2021-10-14 10:42:27 -0700

    fix stray block quotes in episode 13

--- swcarpentry/r-novice-gapminder.R
+++ swcarpentry/r-novice-gapminder.R
@@ -15,3 +15,29 @@ k <- sub(
   '``` ``r "r round(some_value, 2)"`` ```',
   k, fixed = TRUE)
 writeLines(k, f)
+
+# Find the position of a node in the XML document
+find_position <- function(body, node) {
+  lines <- xml2::xml_attr(xml2::xml_children(body), "sourcepos")
+  which(lines %in% xml2::xml_attr(node, "sourcepos"))
+}
+# Episode 13 has a solution block that was not entirely translated, so it needs
+# extra processing to remove the block quotes
+e <- pegboard::Episode$new(fs::path(new, "episodes", "13-dplyr.Rmd"))
+e$confirm_sandpaper()
+blocks <- e$get_blocks() # rogue block quotes
+# Find the adjacent solution
+pos <- pegboard:::get_linestart(blocks[[1]])
+solutions <- e$solutions
+spos <- purrr::map_int(solutions, ~rev(pegboard:::get_linestart(.x))[1])
+the_solution <- solutions[[which.min(pos - spos[spos < pos])]]
+the_fence <- the_solution[length(the_solution)]
+
+# remove the block quotes and append a solution
+chillins <- purrr::map(blocks, pegboard:::elevate_children)
+end_block <- find_position(e$body, chillins[[length(chillins)]])
+# remove the code fence
+xml2::xml_remove(the_fence)
+e$add_md(xml2::xml_text(the_fence), where = end_block)
+
+e$write(path = fs::path(new, "episodes"), format = "Rmd")
