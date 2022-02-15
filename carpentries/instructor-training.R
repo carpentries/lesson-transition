b90ec7248faac09c944e6e7a3f4cba3c395bde23
Zhian N. Kamvar <zkamvar@gmail.com>
2022-02-15 11:41:01 -0800

    convert instructor training; update renv

--- carpentries/instructor-training.R
+++ carpentries/instructor-training.R
@@ -1,1 +1,39 @@
-# script
+l <- pegboard::Lesson$new(new, jekyll = FALSE)
+links <- l$validate_links()
+have_this_url <- grep("carpentries.github.io/instructor-training/", links$orig)
+selfies <- links[have_this_url, , drop = FALSE]
+become_self_aware <- function(node) {
+  dst <- xml2::url_parse(xml2::xml_attr(node, "destination"))$path
+  if (fs::path_file(dst) == "index.html") {
+    dst <- fs::path_dir(dst)
+  }
+  dst <- fs::path_ext_set(dst, "md")
+  dst <- sub("[/]?instructor-training/", "", dst)
+  xml2::xml_set_attr(node, "destination", dst)
+}
+write_out <- function(path) {
+  ep <- fs::path_file(path)
+  out <- fs::path(new, "episodes")
+  type <- fs::path_ext(ep)
+  l$episodes[[ep]]$write(path = out, format = type)
+}
+purrr::walk(selfies$node, become_self_aware)
+purrr::walk(selfies$filepath, write_out)
+
+oextra <- function(x) fs::path(new, "_extras", x)
+instr <- fs::path(new, "instructors")
+learn <- fs::path(new, "learners")
+rewrite(oextra("checkout.md"), learn)
+rewrite(oextra("demo_lessons.md"), learn)
+rewrite(oextra("demos_rubric.md"), learn)
+fs::file_move(oextra("glossary.md"), fs::path(learn, "reference.md"))
+rewrite(oextra("members.md"), learn)
+rewrite(oextra("training_calendar.md"), learn)
+rewrite(oextra("fromthelearners.md"), learn)
+set_learners(new, c("training_calendar.md", "checkout.md", "demo_lessons.md", "demos_rubric.md", "members.md", "fromthelearners.md", "reference.md"), write = TRUE)
+
+rewrite(oextra("registration_confirmation.md"), instr)
+fs::file_move(oextra("etherpad.md"), instr)
+rewrite(oextra("additional_exercises.md"), instr)
+rewrite(oextra("icebreakers.md"), instr)
+set_instructors(new, c("icebreakers.md", "additional_exercises.md", "etherpad.md", "registration_confirmation.md"), write = TRUE)
