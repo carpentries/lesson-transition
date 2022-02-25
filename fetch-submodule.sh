136131259514a074c84375c71c8436dd1a60d848
Zhian N. Kamvar <zkamvar@gmail.com>
2022-02-25 14:24:13 -0800

    submodules can DIAF

--- fetch-submodule.sh
+++ fetch-submodule.sh
@@ -4,12 +4,12 @@ REPO=${1%.*}
 TOKEN=$(./pat.sh)
 
 echo -e "\033[1mChecking \033[38;5;208m${REPO}\033[0;00m...\033[22m"
-BRANCH=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${TOKEN}" https://api.github.com/repos/${REPO} | jq -r '.default_branch')
+BRANCH=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${TOKEN}" https://api.github.com/repos/${REPO%%[/]} | jq -r '.default_branch')
 
 if [[ $(grep -c ${REPO} .gitmodules) -eq 0 ]]; then
     # if the repository does not exist, then we need to create it
     echo -e "... \033[1mCreating new submodule in \033[38;5;208m${REPO}\033[0;00m\033[22m"
-    git submodule add -b ${BRANCH} https://github.com/${REPO} ${REPO} 2> /dev/null
+    git submodule add --force -b ${BRANCH} https://github.com/${REPO} ${REPO} #2> /dev/null
 else
     echo -e "... \033[1mUpdating \033[38;5;208m${REPO}\033[0;00m...\033[22m"
 fi
