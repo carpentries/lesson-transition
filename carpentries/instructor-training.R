5bd03bae1eca0ec88ab522b7c08796e826dfa440
Zhian N. Kamvar <zkamvar@gmail.com>
2022-03-03 13:11:19 -0800

    add some notes to the instructor training

--- carpentries/instructor-training.R
+++ carpentries/instructor-training.R
@@ -19,15 +19,67 @@ write_out <- function(path) {
   type <- fs::path_ext(ep)
   l$episodes[[ep]]$write(path = out, format = type)
 }
+cli::cli_alert("fixing link outs to official site")
 purrr::walk(selfies$node, become_self_aware)
+
 purrr::walk(selfies$filepath, write_out)
 
+cli::cli_alert("adding instructor notes to How We Operate")
+
+titles <- purrr::map_chr(l$episodes, ~.x$get_yaml()$title)
+oper <- l$episodes[[grep("How We Operate", titles)]]
+oper_note <- r'{
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: instructor
+  
+- CK: Not an "official" exercise, but after explaining the workshops and how to run them,
+  go around the room, asking each person if they have a question + then answer them.
+
+- Exercise: Creating a Workshop Website
+  
+  - CK: This takes some time, so some people opt to skip this section.  Inevitably,
+    when working with a group of mixed experience with GitHub, some will be able
+    to zip through this exercise, where others will struggle.  We **have** gotten
+    positive feedback about this exercise as well, where learners felt like it was
+    a valuable experience. Can be especially valuable for groups that will probably
+    be running workshops on their own (so open trainings, or trainings for folks
+    who are ready to get started right away).
+
+- CK: The coffee break after this would be a great time for an "Ask and Offer" if the
+  group is interested.
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+}'
+oper$add_md(oper_note, 1)
+write_out(oper$path)
+
+
 idx <- pegboard::Episode$new(fs::path(new, "index.md"))
+idx_note <- r'{
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: instructor
+
+### A note about instructor view
+
+The instructor view has a few features that the learner view does not have:
+
+1. instructor notes like this one where instructors can provide guidance about
+   how the particular episode is taught in practice **(you can find an example of
+   this in the [How We Operate episode](15-carpentries.md))**.
+2. aggregated timing estimates
+3. a schedule integrated into the index page
+
+You can enter instructor view at any time by selecting the dropdown menu at the
+top right of the page or you can edit the URL and place `/instructor/` before
+the html page. 
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+}'
 idx$confirm_sandpaper()
+idx$add_md(idx_note, 1)
 ilinks <- idx$validate_links()
 iselfies <- ilinks[grep("carpentries.github.io/instructor-training/", ilinks$orig), , drop = FALSE]
 purrr::walk(iselfies$node, become_self_aware)
 idx$write(new, format = "md")
+
 
 # modify break episodes
 cli::cli_alert("modifying breaks episodes")
@@ -50,6 +102,7 @@ rewrite(oextra("fromthelearners.md"), learn)
 set_learners(new, c("training_calendar.md", "checkout.md", "demo_lessons.md", "demos_rubric.md", "members.md", "fromthelearners.md", "reference.md"), write = TRUE)
 
 rewrite(oextra("registration_confirmation.md"), instr)
+fs::file_move(fs::path(instr, "guide.md"), fs::path(instr, "instructor-notes.md"))
 fs::file_move(oextra("etherpad.md"), instr)
 rewrite(oextra("additional_exercises.md"), instr)
 rewrite(oextra("icebreakers.md"), instr)
