691be6d6fe393b4b757025705519fc767650e7e2
Zhian N. Kamvar <zkamvar@gmail.com>
2022-05-17 17:32:51 -0700

    fix problems with RRVG

--- datacarpentry/r-raster-vector-geospatial.R
+++ datacarpentry/r-raster-vector-geospatial.R
@@ -1,1 +1,19 @@
-# script
+fix_setup_link <- function(ep) {
+  setup <- ep$code[[which(xml2::xml_attr(ep$code, "name") == "setup")[1]]]
+  txt <- xml2::xml_text(setup)
+  xml2::xml_set_text(setup, gsub("\\.\\./setup.R", "setup.R", txt))
+  ep$write(fs::path(new, "episodes"), format = "Rmd")
+}
+ep <- old_lesson$episodes[["03-raster-reproject-in-r.Rmd"]]
+is_solution <- ep$get_blocks()[[1]]
+xml2::xml_set_attr(is_solution, "ktag", "{: .solution}")
+pegboard:::replace_with_div(is_solution)
+ep$label_divs()
+
+purrr::walk(old_lesson$episodes, fix_setup_link)
+
+setup <- readLines(fs::path(new, "setup.R"))
+unlink(fs::path(new, "setup.R"))
+
+writeLines(c("options(timeout = max(300, getOption('timeout')))", setup), 
+  fs::path(new, "episodes", "setup.R"))
