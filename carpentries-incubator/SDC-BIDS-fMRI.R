67a60554803f5bac060b4e1fe6a473e85fa8b4f0
Zhian N. Kamvar <zkamvar@gmail.com>
2022-09-23 15:49:47 -0700

    fix parsing error in SDC-BIDS-fMRI

--- carpentries-incubator/SDC-BIDS-fMRI.R
+++ carpentries-incubator/SDC-BIDS-fMRI.R
@@ -1,1 +1,10 @@
-# script
+# Episode 2 has a problem with the translation because they have a double 
+# block quote when they only needed a single block quote. 
+f <- fs::path(new, "episodes", "02-exploring-fmriprep.md")
+k <- readLines(f)
+k <- sub( '^[>][>]', '>', k)
+writeLines(k, f)
+
+e <- pegboard::Episode$new(fs::path(new, "episodes", "02-exploring-fmriprep.md"))
+transform(e)
+
