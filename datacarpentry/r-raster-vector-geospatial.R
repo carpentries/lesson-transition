5f4fca3382b522e777cea8128b683c86e8329dbe
Zhian N. Kamvar <zkamvar@gmail.com>
2022-10-31 10:06:30 -0700

    release r-raster-vector-geospatial to pre-beta

--- datacarpentry/r-raster-vector-geospatial.R
+++ datacarpentry/r-raster-vector-geospatial.R
@@ -10,10 +10,35 @@ xml2::xml_set_attr(is_solution, "ktag", "{: .solution}")
 pegboard:::replace_with_div(is_solution)
 ep$label_divs()
 
-purrr::walk(old_lesson$episodes, fix_setup_link)
+# We have some links that were not detected in our setup 
+transform_missing_links <- function(ep) {
+  braces <- ".//md:text[contains(text(), '{{')]"
+  to_fix <- xml2::xml_find_all(ep$body, braces, ep$ns)
+  if (length(to_fix) == 0L) {
+    # if we did not find anything, bail early
+    return(to_fix)
+  }
+  txt <- xml2::xml_text(to_fix)
+  # Find the pattern of {{ site.baseurl }}
+  base <- "[{][{]\\s*?site\\.baseurl\\s*?[}][}]"
+  # if it is by itself, it is the index page
+  index <- paste0(base, "[)]")
+  txt <- sub(index, "../index.md)", txt)
+  # otherwise, it is a link to a specific page
+  page  <- paste0(base, "[/]([^/]+?)[/]")
+  txt <- sub(page, "\\1.Rmd", txt)
+  xml2::xml_set_text(to_fix, txt)
+}
 
+fix_raster_episodes <- function(ep) {
+  transform_missing_links(ep)
+  fix_setup_link(ep)
+}
+
+purrr::walk(old_lesson$episodes, fix_raster_episodes)
+
+# Make sure that fetching the data does not timeout 
 setup <- readLines(fs::path(new, "setup.R"))
 unlink(fs::path(new, "setup.R"))
-
 writeLines(c("options(timeout = max(300, getOption('timeout')))", setup), 
   fs::path(new, "episodes", "setup.R"))
