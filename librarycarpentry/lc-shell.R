8ed57a349dc598ce9fbc8e778341692c0926073a
Zhian N. Kamvar <zkamvar@gmail.com>
2022-11-09 10:13:17 -0800

    add transformation for librarycarpentry/lc-shell

--- librarycarpentry/lc-shell.R
+++ librarycarpentry/lc-shell.R
@@ -6,3 +6,21 @@
 # to()       - function that constructs a path to the new lesson
 # old_lesson - a pegboard::Lesson object containing the transformed files from
 #              the old lesson
+
+# Fix setup, which has a bug where there is a missing block quote indentation
+f <- fs::path(old, "setup.md")
+stp <- readLines(f)
+bad <- grep("^~~~", stp)
+stp[bad] <- paste0(">", stp[bad])
+writeLines(stp, f)
+rewrite(f, fs::path(new, "learners"))
+
+# fix episode 4, which had an extra nested block quote
+ep4 <- old_lesson$episodes[[4]]
+# isolate the bad div
+bad_div <- ep4$get_divs(type = "language-bash", include = TRUE)[[1]]
+# remove the fences and rewrite
+n <- length(bad_div)
+xml2::xml_remove(bad_div[c(1, 2, n-1, n)])
+ep4$write(fs::path(new, "episodes"), format = "md")
+
