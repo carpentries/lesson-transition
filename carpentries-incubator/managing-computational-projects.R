1acf8f179b07f89825e0633ca1d82714822956d8
Zhian N. Kamvar <zkamvar@gmail.com>
2023-01-26 11:27:49 -0800

    add managing-computational-projects
    This will address https://github.com/carpentries-incubator/managing-computational-projects/issues/67

---
+++ carpentries-incubator/managing-computational-projects.R
@@ -0,0 +1,8 @@
+# Available variables
+#
+# old        - path to the old lesson
+# from()     - function that constructs a path to the old lesson
+# new        - path to the new lesson
+# to()       - function that constructs a path to the new lesson
+# old_lesson - a pegboard::Lesson object containing the transformed files from
+#              the old lesson
