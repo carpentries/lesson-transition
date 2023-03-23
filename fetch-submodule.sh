551575caed718b75de899ba2ff77587f09dfdc72
Zhian N. Kamvar <zkamvar@gmail.com>
2023-03-23 15:58:34 -0700

    fix ignore criteria; add doc for modules

--- fetch-submodule.sh
+++ fetch-submodule.sh
@@ -1,12 +1,67 @@
 #!/usr/bin/env bash
 
+# Create or update a submodule in this repository
+# 
+# This is intended to reduce the amount of effort needed to create submodules
+# of Carpentries Lessons for conversion. To add a new submodule to this
+# repository, one simply has to add a script with the name of the repository
+# to the folder of the organisation name and this script will take care of
+# the rest.
+#
+# Usage:
+#
+# bash fetch-submodule.sh <org>/<repo>.R
+#
+#
+# Details
+# =======
+#
+# This will behave differently based on these situations:
+#
+#
+# The submodule does not exist in the repository
+# -----------------------------------------------
+#
+# In this case, there are a few options depending on if we have seen this
+# repository before or not.
+#
+# #### We want to add a new lesson
+#
+# When we want a new submodule, that means we just added a new file to the
+# repository and this script will add the submodule in the same folder.
+#
+# #### We explicitly ignore this lesson in `.module-ignore`
+#
+# There are some repositories that we do not want to transform to using The
+# Workbench. To prevent these repositories from cluttering up the submodules,
+# we can add them to the `.module-ignore` file and then the module will be
+# skipped when this script is run
+#
+#
+# #### We have completed a release of this lesson
+#
+# In this case, the lesson has already gone through the release process and
+# is now a Workbench lesson. We no longer need to keep track of this lesson and
+# we can safely remove the module. 
+#
+# The submodule exists in the repository
+# ---------------------------------------
+#
+# If we still want the submodule, then it will be updated. If we DO NOT want
+# the submodule, then it will be removed according to 
+# https://stackoverflow.com/a/1260982/2752888
+#
+# NOTE: the changes will be staged and you will have to write a commit message
+# if the submodule is removed
 REPO="${1%.*}"
 
-BETA="beta/${REPO%%[/]}.json"
-RELEASE="release/${REPO%%[/]}.json"
+BETA="beta/${REPO%%[/]}-invalid.hash"
+RELEASE="release/${REPO%%[/]}-invalid.hash"
 IN_MODULES=$(grep -c ${REPO%%[/]} .gitmodules)
 IN_IGNORE=$(grep -c ${REPO%%[/]} .module-ignore)
 
+# If we are ignoring the repository, or it has been released, then we want to
+# shortcut out of here OR delete it. 
 if [[ "${IN_IGNORE}" -ne 0 || -e "${BETA}" || -e "${RELEASE}" ]]; then
   if [[ ${IN_MODULES} -ne 0 ]]; then
     echo -e "\033[1mRemoving \033[38;5;208m${REPO}\033[0;00m as a submodule\033[22m"
