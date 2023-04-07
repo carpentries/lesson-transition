b2a9752d090a68ea2adb57b5b73abd38246804b1
Zhian N. Kamvar <zkamvar@gmail.com>
2023-04-07 10:11:34 -0700

    allow submodule mistakes to exit gracefully

--- fetch-submodule.sh
+++ fetch-submodule.sh
@@ -83,16 +83,19 @@ TOKEN=$(./pat.sh)
 echo -e "\033[1mChecking \033[38;5;208m${REPO}\033[0;00m...\033[22m"
 BRANCH=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${TOKEN}" https://api.github.com/repos/${REPO%%[/]} | jq -r '.default_branch')
 
+set -e
+
 if [[ $(grep -c ${REPO} .gitmodules) -eq 0 ]]; then
     # if the repository does not exist, then we need to create it
     echo -e "... \033[1mCreating new submodule in \033[38;5;208m${REPO}\033[0;00m\033[22m"
-    git submodule add -b ${BRANCH} https://github.com/${REPO} ${REPO} #2> /dev/null
+    rm -rf "${REPO}"
+    git submodule add -b ${BRANCH} https://github.com/${REPO} ${REPO} || exit 0 #2> /dev/null
 elif [[ ! -e ${REPO}/.git ]]; then
     # if the folder exists, but no repository exists, we need to create it. 
-    git submodule update --init ${REPO}
+    git submodule update --init ${REPO} || exit 0
 else
     echo -e "... \033[1mUpdating \033[38;5;208m${REPO}\033[0;00m...\033[22m"
-    git submodule add --force -b ${BRANCH} https://github.com/${REPO} ${REPO} #2> /dev/null
+    git submodule add --force -b ${BRANCH} https://github.com/${REPO} ${REPO} || exit 0 #2> /dev/null
 fi
 
 # Running a subshell so we don't accidenatlly change our working directory
