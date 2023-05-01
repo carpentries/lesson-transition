f8ed4e09f23e9ec502bd7d5afab9e9e0bdfec0c8
Zhian N. Kamvar <zkamvar@gmail.com>
2023-05-01 08:16:11 -0700

    fix some bugs in genomics-r-intro

--- datacarpentry/genomics-r-intro.R
+++ datacarpentry/genomics-r-intro.R
@@ -20,3 +20,38 @@
 # to         <- function(...) fs::path(new, ...)
 # old_lesson <- pegboard::Lesson$new(new, jekyll = FALSE)
 
+# Episode 07 has a problem with the translation because they used the HTML code
+# for the backtic, but commonmark "helpfully" translated it back to a backtic.
+#
+# Instead, what we can do here is to wrap it in sets of backtics because knitr
+# will check for `r code` and replace that with the output of the code, which
+# means that ``r code`` will become `code`. 
+#
+# markdown allows displaying backtics inside code as long as the number of 
+# backtics are fewer than the number of backtics to define the code element so
+# ``` `code` ``` becomes <code>`code`</code> in the HTML.
+f <- fs::path(new, "episodes", "07-knitr-markdown.Rmd")
+k <- readLines(f)
+k <- sub(
+  '<code>\\`r round(some\\_value, 2)\\`</code>',
+  '``` ``r "r round(some_value, 2)"`` ```',
+  k, fixed = TRUE)
+writeLines(k, f)
+
+codes <- old_lesson$get("code") 
+
+to_fix <- purrr::map(codes, function(x) grepl("../data", xml2::xml_text(x), fixed = TRUE))
+
+purrr::walk2(codes, to_fix, function(blocks, fix) {
+  if (any(fix)) {
+    for (block in blocks[fix]) {
+      txt <- xml2::xml_text(block)
+      xml2::xml_set_text(block, gsub("../data/", "data/", txt))
+    }
+  }
+  return(blocks)
+})
+
+to_write <- purrr::map_lgl(to_fix, any)
+purrr::walk(old_lesson$episodes[to_write], write_out_rmd)
+
