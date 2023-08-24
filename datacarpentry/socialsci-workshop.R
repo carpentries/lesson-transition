fbd4d5d30ce6b5351809aa5c4113ed6df085dc1f
Zhian N. Kamvar <zkamvar@gmail.com>
2023-08-24 15:15:00 -0700

    add script but not working for some reason -_-

--- datacarpentry/socialsci-workshop.R
+++ datacarpentry/socialsci-workshop.R
@@ -20,3 +20,29 @@
 # to         <- function(...) fs::path(new, ...)
 # old_lesson <- pegboard::Lesson$new(new, jekyll = FALSE)
 
+child_from_include <- function(ep) {
+  rlang::inform(c(i = ep$path))
+  # find all the {% include file.ext %} statements
+  includes <- xml2::xml_find_all(ep$body, 
+    ".//md:text[starts-with(text(), '{% include')]", ns = ep$ns)
+  # trim off everything but our precious file path
+  f <- gsub("[%{ }]|include", "", xml2::xml_text(includes))
+  # give it a name 
+  fname <- paste0("include-", fs::path_ext_remove(f))
+  # make sure the file path is correct
+  f <- sQuote(fs::path("files", f), q = FALSE)
+  p <- xml2::xml_parent(includes)
+  xml2::xml_remove(includes)
+  xml2::xml_set_name(p, "code_block")
+  xml2::xml_set_attr(p, "language", "r")
+  xml2::xml_set_attr(p, "child", f)
+  xml2::xml_set_attr(p, "name", fname)
+  path <- fs::path_ext_set(ep$path, "Rmd")
+  fs::file_move(ep$path, path)
+  ep$write(fs::path_dir(path), format = "Rmd")
+}
+
+child_from_include(old_lesson$extra$setup.md)
+child_from_include(old_lesson$extra$"setup-r-workshop.md")
+child_from_include(old_lesson$extra$"setup-python-workshop.md")
+
