9400212673bb79a6308d47d28a4b93bfbc0dfdf1
Zhian N. Kamvar <zkamvar@gmail.com>
2023-08-28 16:00:58 -0700

    add ecology workshop stuff

--- datacarpentry/ecology-workshop.R
+++ datacarpentry/ecology-workshop.R
@@ -20,3 +20,31 @@
 # to         <- function(...) fs::path(new, ...)
 # old_lesson <- pegboard::Lesson$new(new, jekyll = FALSE)
 
+child_from_include <- function(from, to = NULL) {
+  to <- if (is.null(to)) fs::path_ext_set(from, "Rmd") else to
+  rlang::inform(c(i = from))
+  ep <- pegboard::Episode$new(from)
+  # find all the {% include file.ext %} statements
+  includes <- xml2::xml_find_all(ep$body, 
+    ".//md:text[starts-with(text(), '{% include')]", ns = ep$ns)
+  # trim off everything but our precious file path
+  f <- gsub("[%{ }]|include", "", xml2::xml_text(includes))
+  # give it a name 
+  fname <- paste0("include-", fs::path_ext_remove(f))
+  # make sure the file path is correct
+  f <- sQuote(fs::path("files", f), q = FALSE)
+  p <- xml2::xml_parent(includes)
+  xml2::xml_remove(includes)
+  xml2::xml_set_name(p, "code_block")
+  xml2::xml_set_attr(p, "language", "r")
+  xml2::xml_set_attr(p, "child", f)
+  xml2::xml_set_attr(p, "name", fname)
+  fs::file_move(from, to)
+  ep$write(fs::path_dir(to), format = "Rmd")
+}
+
+child_from_include(to("learners/setup.md"))
+child_from_include(to("setup-r-workshop.md"))
+child_from_include(to("setup-python-workshop.md"))
+fs::file_move(to("instructors/data.md"), to("learners/data.md"))
+
